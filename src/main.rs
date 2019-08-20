e18f3a731c6c63b96f7c3a56d7430212cb101db7
Palash Nigam <npalash25@gmail.com>
2019-08-20 21:25:54 +0530

    Add args parse and display function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
 use std::env;
 use std::fs;
+use std::io::Read;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+struct Args {
+    filename: Vec<String>,
+    flags: Vec<String>,
+}
 
-    let flags = &args[1..];
+fn get_args() -> Args {
+    let cmd_args: Vec<String> = env::args().collect();
+    let mut args = Args{
+        filename: Vec::new(),
+        flags: Vec::new(),
+    };
 
-    println!("{:?}", flags);
+    // Separate flags from filename
+    for a in &cmd_args {
+        if a.starts_with("-") {
+            args.flags.push(String::from(a));
+        } else {
+            args.filename.push(String::from(a));
+        }
+    }
+    args
+}
 
-    let contents = fs::read_to_string(&args[args.len()-1]).expect("couldn't read the file");
+
+fn display(file: &String, flags: Vec<String>) -> std::io::Result<()> {
+    let filename = file.clone();
+
+    let mut fobj = fs::File::open(file).expect("couldn't read the file");
+    let mut contents = String::new();
+    fobj.read_to_string(&mut contents)?;
 
     let lines: Vec<&str> = contents.lines().collect();
     let words: Vec<&str> = contents.split_ascii_whitespace().collect();
 
-    println!("chars {} \nlines: {} \nwords: {}", contents.len(), lines.len(), words.len());
+    println!("{} {} {} {}", lines.len(), words.len(), contents.len(), filename);
+    Ok(())
+}
+
+fn main() {
+    let args = get_args();
+
+    for a in &args.filename {
+        let res = display(a, args.flags.clone());
+        match res {
+            Ok(_) => (),
+            Err(_) => (),
+        };
+    }
 }
